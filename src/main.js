dd18821dbad02b6a86bd132e8b11a25ed4f8bfd0
manuhostin <hostimmanu@gmail.com>
2024-08-31 08:58:06 -0300

    bootstrap

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 createApp(App).mount('#app')
