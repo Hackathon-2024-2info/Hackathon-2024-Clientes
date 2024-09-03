487bc68fc3839f8d955b8bf0a81b71f8bdd5b7f6
nicoleabrand <nicoleabrand13@gmail.com>
2024-09-03 13:37:31 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+import Flicking from "@egjs/vue3-flicking";
+import "@egjs/vue3-flicking/dist/flicking.css";
+// Or, if you have to support IE9
+import "@egjs/vue3-flicking/dist/flicking-inline.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component("Flicking", Flicking);
+app.mount("#app");
