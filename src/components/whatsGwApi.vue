e13c44b6e78ad44b7fa9157d7f18697baeab8cf3
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-09-06 16:37:12 -0300

    FEAT: Botao de contato via whatsapp

--- src/components/whatsGwApi.vue
+++ src/components/whatsGwApi.vue
@@ -4,12 +4,10 @@
 <template>
 
 <div class="zapbutton">
- 
     
 <a href="https://wa.me/+5547996105239" target="_blank">
     <img src="./assets/zap.png" width="50" alt="Fale com nois" title="Fale com nois">
 </a>
-
 </div>
 
 
@@ -20,7 +18,7 @@
 .zapbutton{
     position: fixed;
     bottom: 30px;
-    right: 40px;
+    right: 30px;
     z-index: 999;
 }
 </style>
